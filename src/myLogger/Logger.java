da5996119fc540b280cbf9127902967d4d13a7bd
Ann-Marie <anniedillier@gmail.com>
2023-10-07 14:38:57 -0700

    remove method from interface

--- src/myLogger/Logger.java
+++ src/myLogger/Logger.java
@@ -5,13 +5,4 @@ public interface Logger {
 	public void log(String string);
 	public void error(String string);
 	
-	
-//	return a string repeated given number time
-	public static String repeatStr(String str, int n) {
-		String newStr = "";
-		for (int i = 0; i < n; i++) {
-			newStr += str;
-		}
-		return newStr;
-	}
 }
--- src/myLogger/SpaceLogger.java
+++ src/myLogger/SpaceLogger.java
@@ -4,17 +4,17 @@ public class SpaceLogger implements Logger{
 
 	@Override
 	public void log(String string) {
-		System.out.println("***" + spaceStr(string) + "***");
-		
+		System.out.println(spaceStr(string));
 	}
 
 	@Override
 	public void error(String string) {
 		// create a new string
 		spaceStr(string);
-		System.out.println("***ERROR: " + spaceStr(string) + "***");
+		System.out.println("ERROR: "+ spaceStr(string));
 	}
 	
+//	create method to loop through the string and extract each charcter and add to a new string with a space after.
 	public String spaceStr(String string) {
 		String newString = "";
 //		loop and add each letter with apace to the new string
